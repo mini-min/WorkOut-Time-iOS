f5ecd9e50753eeea6853027f898e6e3d316168d2
MinjaeLee <2alswo7@khu.ac.kr>
2023-07-04 18:33:35 +0900

    Design: 스톱워치 화면 구현 (1차)

--- WorkOut-Time-iOS/WorkOut-Time-iOS/Sources/ViewControllers/StopWatchViewController.swift
+++ WorkOut-Time-iOS/WorkOut-Time-iOS/Sources/ViewControllers/StopWatchViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class StopWatchViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - @IBAction
+    @IBAction func lapResetTime(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func startPauseTime(_ sender: Any) {
+    }
+    
 }
